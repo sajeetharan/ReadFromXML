4e8aa5bf30c472298943240ec7757807a8538367
sajeetharan <sajeetharan@outlook.com>
2014-05-20 12:19:28 +0530

    Read values from xml

---
+++ ReadFromXML/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ID = "1";
+            var doc = XDocument.Load("XMLFile1.xml");
+            var emailQuery = from Atleta in doc.Root.Elements("Atleta")
+                             where (string)Atleta.Attribute("Id") == ID
+                             select new
+                             {
+                                 Name = (string)Atleta.Attribute("Nombre"),
+                                 Genre = (string)Atleta.Attribute("Genero")
+                             };
+
+
+        }
+    }
+}
